e64757902119fe7a6a75123b60c405da15fbd936
BrashannnStudios <brashannnstudios@gmail.com>
2026-09-13 13:05:55 -0300

    Update database.py

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ async def init_db():
     global client, db
     client = AsyncIOMotorClient(MONGO_URI)
     db = client["nullspacebot"]
-    print("[OK] Conectado a MongoDB")
+    print("[OK] Conectado a MongoDB", flush=True)
     return db
 
 
